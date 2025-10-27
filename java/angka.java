cf5176c4dc8729573cea89fd3604d25b9248a485
fawwaz06 <muhammadfawazazmi0507@gmail.com>
2025-10-27 11:24:04 +0700

    Add files via upload

---
+++ java/angka.java
@@ -0,0 +1,18 @@
+import java..util.scanner;
+
+public class Main{
+    public static void main (String[]args)
+    scanner sc=new Scanner(system.in)
+    system.out.print("Masukan angka Kedua");
+    int b=sc.nextInt();
+
+    if(a>b)
+     system.out.printIn(a +"lebih besar dari" + b);
+     else (a<b)
+      system.out.printIn(a +"lebih kecil dari" + b);
+      else
+      system.out.printIn(Keduanya sama besar);
+
+      system.out.printIn("Apakah Keduanya sama?" + (a==b))
+}
+}

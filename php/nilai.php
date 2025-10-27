2efa5b177c03c792608ed2149d6b03eefc79100b
fawwaz06 <muhammadfawazazmi0507@gmail.com>
2025-10-27 11:26:19 +0700

    Add files via upload

---
+++ php/nilai.php
@@ -0,0 +1,11 @@
+<?php
+$tugas=readline("Masukan nilai tugas:");
+$uts=readline("Masukan nilai UTS:");
+$uas=readline("Masukan nilai UAS:");
+
+$nilai AKhir=(0.3$tugas) + (0.3*$uts) + (0.4*$uas);
+
+echo"Nilai AKhir=$nilai AKhir\n"
+if ($nilaiAKhir>=60)
+    echo"status:LULUS\n"
+?>
